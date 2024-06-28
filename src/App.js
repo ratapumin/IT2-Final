5ebab568664e4c35edc388114106dffd46143f60
Ratapumin Naudom <ratapumin@gmail.com>
2024-06-28 22:45:40 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import logo from "./img/logo-kathong.png";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div className="content">
+        <div className="box">
+          <h1 className="">WELCOME TO KATHONG COFFEE SHOP</h1>
+          <div className="box-logo">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+          <form className="form">
+            <label>UserId:</label>
+            <input type="text" className="input-box" />
+            <label>Password:</label>
+            <input type="password" className="input-box" />
+            <div className="button-container">
+              <button type="submit" className="center-button">
+                Login
+              </button>
+            </div>
+          </form>
+          <p>
+            <Link to="D:\Project\project\my-react-app\src\Login.jsx">
+              {" "}
+              Link
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import background from "./img/logo-kathong.png";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div
+        className="flex items-center justify-center min-h-screen bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <div className="bg-opacity- p-6 rounded-lg shadow-md">
+          <form className="space-y-5">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                User ID
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-72 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="User ID"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-72 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
