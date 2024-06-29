279872f6ef9dd71305d96656e800c4c1d8f9a6fd
Ratapumin Naudom <ratapumin@gmail.com>
2024-06-29 20:23:09 +0700

    can link page login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-// import logo from "./img/logo-kathong.png";
 import PageHome from "./components/PageHome";
 import PageAbout from "./components/PageAbout";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PageHome />} />
         <Route path="/about" element={<PageAbout />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "../img/logo-kathong.png";
+
 function Login() {
   return (
     <>
