a35b9588d6b7f21cc3ae588bdb206ede820db940
Ratapumin Naudom <ratapumin@gmail.com>
2024-08-10 17:15:04 +0700

    update menu orders

--- src/components/order/Orders.js
+++ src/components/order/Orders.js
@@ -4,25 +4,65 @@ function Orders({ user, products }) {
   const [productCart, setProductCart] = useState([]);
 
   useEffect(() => {
-    if (products && products.length > 0) { // ตรวจสอบว่ามีสินค้าหรือไม่
-      setProductCart(products); // ใช้ products ทั้งหมดแทนการเพิ่มครั้งละ 1
+    if (products && products.length > 0) {
+      setProductCart(products);
     }
   }, [products]);
-console.log('carts pro',productCart)
+
   return (
     <section className="orders">
-      <h1>New Orders</h1>
-      {user && user.user_fname}
+      <div className="content-order">
+        <h1>New Orders</h1>
+        {user && user.user_fname}
 
-      <section className="order-box">
-        {productCart.map((item, index) => (
-          <div className="order-flex" key={item.p_id || index}>
-            <p className="item-box">{item.p_name}</p>
-            <p className="item-box-2">{item.p_price}</p>
-            <p className="item-box-2">{item.quantity}</p>
-          </div>
-        ))}
+        <section className="bg-order">
+          <section className="order-box">
+            {productCart.map((item, index) => (
+              <div className="order-flex" key={item.p_id || index}>
+                <p className="item-box">{item.p_name}</p>
+                <p className="item-box-2">{item.p_price}</p>
+                <p className="item-box-2">{item.quantity}</p>
+              </div>
+            ))}
+          </section>
+
+          <section className="subtotal">
+            <p>Subtotal</p>
+            <p>0</p>
+          </section>
+          <section className="total">
+            <p>Total</p>
+            <p>0</p>
+          </section>
+          <section className="cash">
+            <p>Cash</p>
+            <p>0</p>
+          </section>
+          <section className="change">
+            <p>Change</p>
+            <p>0</p>
+          </section>
+        </section>
+      </div>
+      <section className="order-bottom">
+
+
+
+        <section className="delete">
+          <p>DELETE</p>
+        </section>
+        <section className="delete-all">
+          <p>DELETE ALL</p>
+        </section>
+        <section className='cashh'>
+          <p>CASH</p>
+        </section>
       </section>
+
+
+
+
+
     </section>
   );
 }
