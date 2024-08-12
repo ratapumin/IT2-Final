530c959e3d60028886f8f8d728ed36c393224c80
Ratapumin Naudom <ratapumin@gmail.com>
2024-08-12 19:00:41 +0700

    create member page

--- src/components/order/Members.js
+++ src/components/order/Members.js
@@ -1,11 +1,36 @@
+import './Members.css'
+
+
+
 function Members() {
 
 
 
 
-    return( <>
+    return (<>
 
-        eiei
+        <div className="content-box">
+
+            <section className='members'>
+                MEMBERS
+            </section>
+
+            <section className='ownerlist'>
+                MEMBER LIST
+            </section>
+
+            <button className='btnAdd'>
+                ADD MEMBER
+            </button>
+
+            <button className='btnSearch'>
+                SEARCH
+            </button>
+
+            <section className='box-table'>
+
+            </section>
+        </div>
     </>)
 }
 
